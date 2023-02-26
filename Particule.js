9bb0a09edcaea7a58c20189269091ae8fcf56094
SarahBendj <sarah.asma.bendjema@gmail.com>
2023-02-26 20:25:48 +0100

    galaxy simulation

--- Particule.js
+++ Particule.js
@@ -1,32 +1,38 @@
 export default class Particule{
-    constructor(x , y , radius){
+    constructor(x , y , radius , color ){
         this.x = x;
         this.y = y;
         this.startAngle = 0
         this.radius = radius
-        this.radians = Math.random()* Math.PI*2;
-        this.velocity = 0.05
+        this.color = color
+        this.radians = 2*Math.PI*(Math.random())
+        this.velocity = 0.03
+        this.randomOrbit= (Math.floor(Math.random()*25))
+        this.position = {
+            x : x ,
+            y : y 
+        }
+        console.log(this.position.x)
         
         
-     
-           }
+    }
 
         draw(context){
            this.motion()
-           context.beginPath();
-           context.arc(this.x , this.y ,this.radius ,this.startAngle, 2 * Math.PI )
-           context.fillStyle = 'pink'
-           context.stroke()
+           context.beginPath(); 
+           context.arc(this.x , this.y ,this.radius ,this.startAngle, 2*Math.PI )
+           context.fillStyle = this.color
+           context.fill()
 
 
         }
         motion(){
             this.radians += this.velocity
-            this.x = this.x + Math.cos(this.radians) *5
-            this.y = this.y + Math.sin(this.radians) *5
+            this.x = this.x + Math.cos(this.radians)*this.randomOrbit 
+            this.y = this.y + Math.sin(this.radians)*this.randomOrbit
+             //
+            
             }
-    
- 
         
     }
 
